a794d503ba265bcec8a23e7e3d97b7ea71b3104e
Pavanrajput123 <155573390+Pavanrajput123@users.noreply.github.com>
2024-01-09 20:17:56 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -11,33 +11,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         //1]AppBar
         appBar: AppBar(
-          title: Text('First AppBar'),
+          title: Text('AppBar'),
           centerTitle: true,
           elevation: 25,
           shadowColor: Color.fromARGB(255, 248, 11, 98),
-          backgroundColor: const Color.fromARGB(255, 241, 32, 122),
+          backgroundColor: Color.fromARGB(255, 241, 14, 67),
           toolbarHeight: 70, //height of AppBar
 
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.home,
-              color: Color.fromARGB(255, 7, 7, 7),
-            ),
-          ),
+          leading: Row(children: [
+            IconButton(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+                onPressed: () {}),
+          ]),
+
           actions: [
             IconButton(
               onPressed: () {},
               icon: Icon(
                 Icons.notifications,
-                color: Color.fromARGB(255, 9, 9, 9),
+                color: Color.fromARGB(255, 243, 236, 236),
               ),
             ),
             IconButton(
               onPressed: () {},
               icon: Icon(
                 Icons.share,
-                color: Color.fromARGB(255, 9, 9, 9),
+                color: Color.fromARGB(255, 255, 250, 250),
               ),
             ),
           ],
@@ -46,16 +48,20 @@ class MyApp extends StatelessWidget {
         //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
-          backgroundColor: Color.fromARGB(255, 241, 32, 122),
+          backgroundColor: Color.fromARGB(255, 241, 14, 67),
           child: Icon(Icons.add),
         ),
         body: Center(
-          child: Text(
-            'Add Containt in body',
-            style: TextStyle(fontSize: 20, color: Colors.black),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Add Containt in body',
+              style: TextStyle(
+                  fontSize: 20, color: Color.fromARGB(255, 254, 253, 253)),
+            ),
           ),
         ),
-        backgroundColor: const Color.fromARGB(255, 240, 157, 157),
+        backgroundColor: Color.fromARGB(255, 41, 41, 41),
       ),
     );
   }
